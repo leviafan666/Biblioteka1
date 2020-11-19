d9749d1942833feb83525d31d037f3ab9bdf7d10
Baa1 <baa1_666@mail.ru>
2020-11-20 03:08:58 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../settings/db');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/authorize', (req, res) => {
+  var params = [
+    req.query.login,
+    req.query.password
+  ];
+  db.any("SELECT id FROM users WHERE login = $1 AND password = $2", params)
+  .then(data => {
+    if (data && data.length > 0)
+      res.send(data[0]);
+    else
+      res.send({id:0});
+  })
+  .catch(err => {
+    console.log(err);
+    res.send("Error");
+  });
+});
+
+router.get('/delete', (req, res) => {
+  db.none("DELETE FROM users WHERE id = $1", req.query.id)
+  .then(() => {
+    res.send("OK");
+  })
+  .catch(err => {
+    console.log(err);
+    res.send("Error");
+  });
+});
+
+router.get('/update', (req, res) => {
+  var params = [
+    req.query.login,
+    req.query.password,
+    req.query.age,
+    req.query.id
+  ];
+  db.none("UPDATE users SET login = $1, password = $2, age = $3 WHERE id = $4", params)
+  .then(() => {
+    res.send("OK");
+  })
+  .catch(err => {
+    console.log(err);
+    res.send("Error");
+  });
+});
+
+router.get('/insert', (req, res) => {
+  var params = [
+    req.query.login,
+    req.query.password,
+    req.query.age
+  ];
+  db.none("INSERT INTO users (login, password, age) VALUES ($1, $2, $3)", params)
+  .then(() => {
+    res.send("OK");
+  })
+  .catch(err => {
+    console.log(err);
+    res.send("Error");
+  });
 });
 
 module.exports = router;
